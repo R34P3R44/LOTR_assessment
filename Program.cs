ec39035af36f476fee7fa7e412de8d67f891390b
R34P3R44 <t.adrian88@gmail.com>
2021-10-24 13:49:18 +0100

    refactor - pulled out logic to handle request

--- Program.cs
+++ Program.cs
@@ -58,82 +58,28 @@ namespace LOTRApp
             //Define base Url
             string baseUrl = "https://the-one-api.dev/v2/book/";
             //try and catch block will will catch any exeptions
-            try
-            {
-                //Defining a Http client
-                using (HttpClient client = new HttpClient())
-                {
-                    //Initiating the Get Request with await keyword so the using statement will be executed in order.
-                    //Httprespinsemessage contains status code and data from response.
-                    using (HttpResponseMessage res = await client.GetAsync(baseUrl))
-                    {
-                        res.Headers.Add("Authorization", $"Bearer _7O4nscQGh3XuIJNeHDm");
-                        //Getting content from response and convert it to a C# object
-                        using (HttpContent content = res.Content)
-                        {
-                            //Assigning the content to data variable by converting it to string with await keyword.
-                            var data = await content.ReadAsStringAsync();
 
-                            //Adding if statement to check data isnt null then return log convert whihc was convreted by newtonsoft.
-                            if (data != null)
-                            {
-                                var dataObj = JObject.Parse(data);
-                                //Logging data to console
-                                Console.WriteLine(dataObj["docs"]);
-                                Console.WriteLine("Type 'book', 'books', 'chapter' or press 'q' if you want to quit");
-                            }
-                            else
-                            {
-                                Console.WriteLine("NO Content---------");
-                            }
-                        }
-                    }
-                }
-            } catch(Exception exception)
-            {
-                Console.WriteLine("Exception Hit----------");
-                Console.WriteLine(exception);
-            }
+            sendParsePrintRequest(baseUrl);
         }
+
         public static async void GetOneBook(string id)
         {
-            string baseURL = $"https://the-one-api.dev/v2/book/{id}/";
-            try { 
-                using (HttpClient client = new HttpClient())
-                {
-                    using (HttpResponseMessage res = await client.GetAsync(baseURL))
-                    {
-                        res.Headers.Add("Authorization", $"Bearer _7O4nscQGh3XuIJNeHDm");
-                        using (HttpContent content = res.Content)
-                        {
-                            string data = await content.ReadAsStringAsync();
-                            if (data != null)
-                            {
-                                var dataObj = JObject.Parse(data);
-                                Console.WriteLine(dataObj["docs"]);
-                                Console.WriteLine("Type 'book', 'books', 'chapter' or press 'q' if you want to quit");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Data is null!");
-                            }
-                        }
-                    }
-                }
-            } catch(Exception exception) {
-                Console.WriteLine(exception);
-            }
+            string baseUrl = $"https://the-one-api.dev/v2/book/{id}/";
+
+            sendParsePrintRequest(baseUrl);
         }
         public static async void GetBookChapters(string id)
         {
+            string baseUrl = $"https://the-one-api.dev/v2/book/{id}/chapter";
 
-            string baseURL = $"https://the-one-api.dev/v2/book/{id}/chapter";
-
+            sendParsePrintRequest(baseUrl);
+        }
+        public static async void sendParsePrintRequest(string baseUrl)
+        {
             try { 
-
                 using (HttpClient client = new HttpClient())
                 {
-                    using (HttpResponseMessage res = await client.GetAsync(baseURL))
+                    using (HttpResponseMessage res = await client.GetAsync(baseUrl))
                     {
                         res.Headers.Add("Authorization", $"Bearer _7O4nscQGh3XuIJNeHDm");
                         using (HttpContent content = res.Content)
